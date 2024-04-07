b5823096212aba28a71646c0278e70db5ab92d74
Fuad Bader <fuad.bader@outlook.com>
2024-04-07 15:21:02 +0300

    some minor visual fixes

--- src/taskpane/components/Register.jsx
+++ src/taskpane/components/Register.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "../../api/axios";
 // import type { FieldProps } from "@fluentui/react-components";
 import { Field, Input } from "@fluentui/react-components";
+import { InfoLabel } from "@fluentui/react-components";
 
 const USER_REGEX = /^[A-z0-9._%+-]+@[A-z0-9.-]+\.[A-z]{2,}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -107,43 +108,31 @@ const Register = () => {
             <h1>Register</h1>
           </center>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="username">
-              Username:
-              <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
-              <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
-            </label>
-            <input
-              type="text"
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setUser(e.target.value)}
-              value={user}
-              required
-              aria-invalid={validName ? "false" : "true"}
-              aria-describedby="uidnote"
-              onFocus={() => setUserFocus(true)}
-              onBlur={() => setUserFocus(false)}
-            />
-            <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
-              <FontAwesomeIcon icon={faInfoCircle} />
-              4 to 24 characters.
-              <br />
-              Must begin with a letter.
-              <br />
-              Letters, numbers, underscores, hyphens allowed.
-            </p>
+            <Field label="User Name" validationState="success" validationMessage="This is a success message.">
+              <Input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+              />
+            </Field>
+            <InfoLabel
+              info={
+                <>
+                  The Username must be 4 to 24 characters and must begin with a letter, Letters, numbers, underscores,
+                  hyphens allowed.
+                </>
+              }
+            ></InfoLabel>
 
-            <label htmlFor="password">
-              Password:
-              <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
-              <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
-            </label>
-            <Field
-              label="Example field"
-              validationState="success"
-              validationMessage="This is a success message."
-            >
+            <Field label="Password" validationState="success" validationMessage="This is a success message.">
               <Input
                 type="password"
                 id="password"
@@ -156,33 +145,33 @@ const Register = () => {
                 onBlur={() => setPwdFocus(false)}
               />
             </Field>
-            <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-              <FontAwesomeIcon icon={faInfoCircle} />
-              8 to 24 characters.
-              <br />
-              Must include uppercase and lowercase letters, a number and a special character.
-              <br />
-              Allowed special characters: <span aria-label="exclamation mark">!</span>{" "}
-              <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span>{" "}
-              <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
-            </p>
+            <InfoLabel
+              info={
+                <>
+                  The Password must be 8 to 24 characters and Must include uppercase and lowercase letters, a number and
+                  a special character. Allowed special characters: ! @ # $ %
+                </>
+              }
+            ></InfoLabel>
 
             <label htmlFor="confirm_pwd">
               Confirm Password:
               <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
               <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
             </label>
-            <input
-              type="password"
-              id="confirm_pwd"
-              onChange={(e) => setMatchPwd(e.target.value)}
-              value={matchPwd}
-              required
-              aria-invalid={validMatch ? "false" : "true"}
-              aria-describedby="confirmnote"
-              onFocus={() => setMatchFocus(true)}
-              onBlur={() => setMatchFocus(false)}
-            />
+            <Field label="Password" validationState="success" validationMessage="This is a success message.">
+              <Input
+                type="password"
+                id="confirm_pwd"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
+                required
+                aria-invalid={validMatch ? "false" : "true"}
+                aria-describedby="confirmnote"
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+              />
+            </Field>
             <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
               <FontAwesomeIcon icon={faInfoCircle} />
               Must match the first password input field.
